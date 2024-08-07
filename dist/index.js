cbb8e19604808fe3a6a839c0d6a348b47fea6c6e
Tusharjamdade <tusharnjamdade@gmail.com>
2024-08-07 21:16:44 +0530

    Changes files

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,24 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_1 = __importDefault(require("http"));
-const express_1 = __importDefault(require("express"));
-const socket_io_1 = require("socket.io");
-const cors_1 = __importDefault(require("cors"));
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000;
-const app = (0, express_1.default)();
-const server = http_1.default.createServer(app);
-const io = new socket_io_1.Server(server);
-app.use((0, cors_1.default)());
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.json({
         msg: "hi there"
     });
 });
+
 server.on("connect", () => {
     console.log("connected");
 });
+
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}.`));
